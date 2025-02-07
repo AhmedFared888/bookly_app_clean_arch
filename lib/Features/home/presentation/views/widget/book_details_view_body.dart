39e031f75c4c60c825205f9ef2e3329e3ac76e33
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-02-07 13:45:36 -0800

    resize book image and add title and subTitle to book details view

--- lib/Features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,27 +1,41 @@
+import 'package:bookly_app_clean_arch/Core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../Core/utils/assets.dart';
 import 'custom_book_details_app_bar.dart';
+import 'featured_books_List_view_item.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Column(
       children: [
         const CustomBookDetailsAppBar(),
-        AspectRatio(
-          aspectRatio: 4 / 5,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: const DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                  AssetsData.testImage,
-                ),
-              ),
+        const SizedBox(
+          height: 33,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .25),
+          child: const FeaturedBooksListViewItem(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          'The Jungel Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
             ),
           ),
         ),
--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -3,37 +3,45 @@ import 'package:bookly_app_clean_arch/Features/home/presentation/views/widget/be
 import 'package:bookly_app_clean_arch/Features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:bookly_app_clean_arch/Features/home/presentation/views/widget/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../Core/utils/app_router.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
         SliverToBoxAdapter(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomAppBar(),
-              FeaturedBooksListView(),
-              SizedBox(
+              const CustomAppBar(),
+              GestureDetector(
+                onTap: () {
+                  GoRouter.of(context).push(AppRouter.kBookDetailsView);
+                },
+                child: const FeaturedBooksListView(),
+              ),
+              const SizedBox(
                 height: 49,
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 30),
                 child: Text(
                   'Best Seller',
                   style: Styles.textStyle18,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10.0,
               ),
             ],
           ),
         ),
-        SliverFillRemaining(
+        const SliverFillRemaining(
           child: BestSellerListView(),
         ),
       ],
